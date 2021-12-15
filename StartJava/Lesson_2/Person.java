b1f00624aa4c8ec1218be0142c3a4a511fc88a9e
Djabrail <jabrail8@mail.ru>
2021-12-16 00:10:56 +0300

    Lesson 2. HW 2

---
+++ StartJava/Lesson_2/Person.java
@@ -0,0 +1,32 @@
+public class Person {
+
+    String sex = "male";
+    String name = "Ivan";
+    String height = 180;
+    String weight = 90;
+    String age = 20;
+
+    void go() {
+        System.out.println("Im go");
+    }
+
+    void sit() {
+        System.out.println("Im sit");
+    }
+
+    void go() {
+        System.out.println("Im go");
+    }
+
+    void run() {
+        System.out.println("Im run");
+    }
+
+    void speak() {
+        System.out.println("Im speak");
+    }
+
+    void learnJava() {
+        System.out.println("Im learn java");
+    }
+}
